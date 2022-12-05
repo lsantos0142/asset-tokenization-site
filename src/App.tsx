d04f07ce064859f18ccba68662806a7b1d71ddc2
Lucas <lucasnego0142@gmail.com>
2022-12-05 20:27:53 -0300

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Banner from "./assets/Banner.pdf";
 import Monografia from "./assets/Monografia.pdf";
 import PressRelease from "./assets/PressRelease.pdf";
@@ -9,16 +8,16 @@ function App() {
         <div className="App">
             <Navbar />
 
-            <h1 className="text-center py-5">
+            <h1 className="text-center p-5">
                 AUMENTO DA LIQUIDEZ DE ATIVOS IMOBILIÁRIOS EM APLICAÇÕES DE
                 EMPRÉSTIMOS E ALUGUÉIS POR MEIO DA TOKENIZAÇÃO
             </h1>
 
             <div className="container" id="about">
-                <h2 className="col-sm text-center m-auto py-3">
+                <h2 className="col-md text-center m-auto py-3">
                     Sobre o Projeto
                 </h2>
-                <div className="col-sm py-3">
+                <div className="col-md py-3">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Ipsum cupiditate maiores sed cum accusantium magnam fuga
                     adipisci nesciunt eius fugit! Maxime quisquam autem odit
@@ -28,8 +27,8 @@ function App() {
 
             <div className="container" id="motivation">
                 <div className="row p-3 gap-5">
-                    <h2 className="col-sm text-center m-auto">Motivação</h2>
-                    <div className="col-sm">
+                    <h2 className="col-md text-center m-auto">Motivação</h2>
+                    <div className="col-md">
                         Lorem ipsum dolor, sit amet consectetur adipisicing
                         elit. Ipsum cupiditate maiores sed cum accusantium
                         magnam fuga adipisci nesciunt eius fugit! Maxime
@@ -40,19 +39,19 @@ function App() {
 
             <div className="container" id="objective">
                 <div className="row p-3 gap-5">
-                    <div className="col-sm">
+                    <div className="col-md">
                         Lorem ipsum dolor, sit amet consectetur adipisicing
                         elit. Ipsum cupiditate maiores sed cum accusantium
                         magnam fuga adipisci nesciunt eius fugit! Maxime
                         quisquam autem odit fugit fuga? Amet porro natus earum!
                     </div>
-                    <h2 className="col-sm text-center m-auto">Objetivo</h2>
+                    <h2 className="col-md text-center m-auto">Objetivo</h2>
                 </div>
             </div>
 
             <div className="container" id="results">
-                <h2 className="col-sm text-center m-auto py-3">Resultados</h2>
-                <div className="col-sm py-3">
+                <h2 className="col-md text-center m-auto py-3">Resultados</h2>
+                <div className="col-md py-3">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Ipsum cupiditate maiores sed cum accusantium magnam fuga
                     adipisci nesciunt eius fugit! Maxime quisquam autem odit
@@ -61,8 +60,8 @@ function App() {
             </div>
 
             <div className="container" id="documentation">
-                <h2 className="col-sm text-center m-auto py-3">Documentação</h2>
-                <div className="col-sm py-3">
+                <h2 className="col-md text-center m-auto py-3">Documentação</h2>
+                <div className="col-md py-3">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Ipsum cupiditate maiores sed cum accusantium magnam fuga
                     adipisci nesciunt eius fugit! Maxime quisquam autem odit
