360edd61015ac7953b6fda3c2f7e2a9adcc69737
Lucas <lucasnego0142@gmail.com>
2022-12-05 18:24:51 -0300

    update banner and press release

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Banner from "./assets/Banner.pdf";
 import Monografia from "./assets/Monografia.pdf";
