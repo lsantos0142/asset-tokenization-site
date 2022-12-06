405ebf7d8859e3bf6197ac11dde18c46df80fa85
Lucas <lucasnego0142@gmail.com>
2022-12-05 22:36:49 -0300

    change some texts

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,12 @@ function App() {
                     Sobre o Projeto
                 </h2>
                 <div className="col-md py-3">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Ipsum cupiditate maiores sed cum accusantium magnam fuga
-                    adipisci nesciunt eius fugit! Maxime quisquam autem odit
-                    fugit fuga? Amet porro natus earum!
+                    <p style={{ textIndent: "50px" }}>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Ipsum cupiditate maiores sed cum accusantium
+                        magnam fuga adipisci nesciunt eius fugit! Maxime
+                        quisquam autem odit fugit fuga? Amet porro natus earum!
+                    </p>
                 </div>
             </div>
 
@@ -30,10 +32,20 @@ function App() {
                 <div className="row p-3 gap-5">
                     <h2 className="col-md text-center m-auto">Motivação</h2>
                     <div className="col-md">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Ipsum cupiditate maiores sed cum accusantium
-                        magnam fuga adipisci nesciunt eius fugit! Maxime
-                        quisquam autem odit fugit fuga? Amet porro natus earum!
+                        <p style={{ textIndent: "50px" }}>
+                            Atualmente, ativos imobiliários possuem liquidez
+                            muito reduzida devido à dificuldade de transferência
+                            entre os proprietários resultante de diversos
+                            procedimentos legais no processo e de altos valores
+                            envolvidos. Dentro deste contexto, como uma
+                            alternativa para ganho de liquidez, surge a proposta
+                            de utilizar frações destes ativos em diferentes
+                            cenários, como garantias de empréstimos e
+                            comercialização, por meio da tecnologia de
+                            blockchain, para garantir segurança, auditabilidade
+                            e transparência das operações envolvendo estes
+                            ativos.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -44,10 +56,36 @@ function App() {
                 <div className="row p-3 gap-5 flex-row-reverse">
                     <h2 className="col-md text-center m-auto ">Objetivo</h2>
                     <div className="col-md">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Ipsum cupiditate maiores sed cum accusantium
-                        magnam fuga adipisci nesciunt eius fugit! Maxime
-                        quisquam autem odit fugit fuga? Amet porro natus earum!
+                        <p style={{ textIndent: "50px" }}>
+                            O objetivo deste projeto foi desenvolver uma
+                            plataforma digital (web app) que associa ativos
+                            imobiliários a tokens numa blockchain Ethereum
+                            (processo conhecido como tokenização) e que se
+                            propõe a fornecer funcionalidades que viabilizam o
+                            aumento da liquidez dos ativos imobiliários
+                            tokenizados, sendo as principais delas:
+                        </p>
+                        <li>
+                            Transacionar imóveis de forma fracionada ou completa
+                            por meio de um marketplace;
+                        </li>
+                        <li>
+                            Registrar porcentagens de imóveis como garantia de
+                            um ou mais empréstimos;
+                        </li>
+                        <li>
+                            Registrar pagamentos de aluguéis dos imóveis, que
+                            serão distribuídos de forma proporcional à
+                            porcentagem de posse de cada um dos proprietários
+                        </li>
+                        <br />
+                        <p style={{ textIndent: "50px" }}>
+                            Através da utilização do blockchain Ethereum para a
+                            tokenização e o registro das operações realizadas
+                            com o imóvel, a plataforma confere imutabilidade,
+                            auditabilidade e transparência aos ativos
+                            imobiliários tokenizados.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -57,10 +95,16 @@ function App() {
             <div className="container px-5" id="results">
                 <h2 className="col-md text-center m-auto py-3">Resultados</h2>
                 <div className="col-md py-3">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Ipsum cupiditate maiores sed cum accusantium magnam fuga
-                    adipisci nesciunt eius fugit! Maxime quisquam autem odit
-                    fugit fuga? Amet porro natus earum!
+                    <p style={{ textIndent: "50px" }}>
+                        Como resultado final, foi obtida com sucesso uma
+                        plataforma digital que envia smart contracts para a
+                        blockchain para tokenizar ativos imobiliários e que
+                        viabiliza posse e operações com porcentagens destes
+                        ativos, podendo ser expandida para abranger outras
+                        funcionalidades como, por exemplo, cenários de
+                        financiamento de imóveis, crowdfunding e outras diversas
+                        aplicações.
+                    </p>
                 </div>
             </div>
 
@@ -68,9 +112,11 @@ function App() {
 
             <div className="container px-5" id="documentation">
                 <h2 className="col-md text-center m-auto py-3">Documentação</h2>
-                <div className="col-md py-3">
-                    Esta seção contém os documentos desenvolvidos ao longo do
-                    projeto.
+                <div className="col-md">
+                    <p style={{ textIndent: "50px" }}>
+                        Esta seção contém os documentos desenvolvidos ao longo
+                        do projeto.
+                    </p>
                 </div>
             </div>
 
